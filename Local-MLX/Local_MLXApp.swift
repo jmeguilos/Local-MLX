9b8e445375488d563874b54f174460d69c43e644
Marvin <jmeguilos@gmail.com>
2026-02-27 14:14:47 -0800

    Initial Commit

---
+++ Local-MLX/Local_MLXApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_MLXApp.swift
+//  Local-MLX
+//
+//  Created by Jules Marvin Eguilos on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Local_MLXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
